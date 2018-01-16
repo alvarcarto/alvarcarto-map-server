81a501d8015eb2e47667559956a4c3aac580bb3c
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2018-01-16 22:59:02 +0200

    Fix alvar repo installing

--- tasks/install-alvar-repos.sh
+++ tasks/install-alvar-repos.sh
@@ -10,25 +10,28 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
 cd $HOME
-git clone https://alvarcarto:b87a485f25d9ea03b77f46deb9348eb541020c13@github.com/kimmobrunfeldt/alvarcarto-render-service.git
+git clone https://alvarcarto-integration:fab7f21687f2cea5dfb2971ea69821b5e5cb87a2@github.com/kimmobrunfeldt/alvarcarto-render-service.git
 cd alvarcarto-render-service
+nvm install 6.9.4
+nvm use 6.9.4
 npm install
 npm run build
+npm run build-posters
 
 cd $HOME
-git clone https://alvarcarto:b87a485f25d9ea03b77f46deb9348eb541020c13@github.com/kimmobrunfeldt/alvarcarto-cartocss.git
+git clone https://alvarcarto-integration:fab7f21687f2cea5dfb2971ea69821b5e5cb87a2@github.com/kimmobrunfeldt/alvarcarto-cartocss.git
 cd alvarcarto-cartocss
-nvm install 6.9.4
+nvm use 6.9.4
 npm install
+npm run build
 mkdir -p $HOME/mapnik-styles
-./node_modules/.bin/carto styles/bw/project.mml > $HOME/mapnik-styles/bw.xml
+cp -r dist/*.xml $HOME/mapnik-styles
 cp -r $HOME/osm/openstreetmap-carto/data $HOME/mapnik-styles
 
 cd $HOME
 mkdir -p $ALVAR_MAP_SERVER_DATA_DIR/tiles/
-git clone https://alvarcarto:b87a485f25d9ea03b77f46deb9348eb541020c13@github.com/kimmobrunfeldt/alvarcarto-tile-service.git
+git clone https://alvarcarto-integration:fab7f21687f2cea5dfb2971ea69821b5e5cb87a2@github.com/kimmobrunfeldt/alvarcarto-tile-service.git
 cd alvarcarto-tile-service
-
 nvm use 6.9.4
 nvm alias default 6.9.4
 echo "nvm use 6.9.4" >> ~/.bashrc
