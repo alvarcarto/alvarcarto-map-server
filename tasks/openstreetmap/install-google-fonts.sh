0bb620f62083813b3ea8e58e46ad7bb680db2574
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2018-01-17 22:28:35 +0200

    Lock google fonts to certain commit, later changes had made Josefin Sans thicker

--- tasks/openstreetmap/install-google-fonts.sh
+++ tasks/openstreetmap/install-google-fonts.sh
@@ -4,7 +4,11 @@
 # Modified by Andrew http://www.webupd8.org <andrew@webupd8.org>
 # Copied from: https://raw.githubusercontent.com/hotice/webupd8/c9bffcd240c71e2cba7aaaf5c28713d5e2fcda78/install-google-fonts
 
-_wgeturl="https://github.com/google/fonts/archive/master.tar.gz"
+set -e
+set -x
+
+_branch="78303685d514cb0e80efc82538aa9818cdd95294"
+_wgeturl="https://github.com/google/fonts/archive/$_branch.tar.gz"
 _gf="google-fonts"
 
 # install wget
@@ -23,7 +27,7 @@ echo "Creating the /usr/share/fonts/truetype/$_gf folder"
 sudo mkdir -p /usr/share/fonts/truetype/$_gf
 
 echo "Installing all .ttf fonts in /usr/share/fonts/truetype/$_gf"
-find $PWD/fonts-master/ -name "*.ttf" -exec sudo install -m644 {} /usr/share/fonts/truetype/google-fonts/ \; || return 1
+find "$PWD/fonts-$_branch/" -name "*.ttf" -exec sudo install -m644 {} /usr/share/fonts/truetype/google-fonts/ \; || return 1
 
 echo "Updating the font cache"
 fc-cache -f > /dev/null
