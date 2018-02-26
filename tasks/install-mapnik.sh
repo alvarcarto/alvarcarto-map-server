13f1426916a58af8164765afb4fc88b6d33ccfb6
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2018-02-26 19:32:17 +0200

    Remove test phase from mapnik files, we have our own test set anyways

--- tasks/install-mapnik.sh
+++ tasks/install-mapnik.sh
@@ -31,7 +31,7 @@ sudo apt-get install -y python python-dev curl zlib1g-dev clang make pkg-config 
 source bootstrap.sh
 ./configure CUSTOM_CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0"
 JOBS=8 make
-make test
+
 sudo make install
 
 sudo apt-get install -y mapnik-utils
