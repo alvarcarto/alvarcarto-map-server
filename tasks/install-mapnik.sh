efb607033e0af0c6c627769c61d2119be3a5148e
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2018-02-26 02:24:23 +0200

    Try to fix mapnik installation

--- tasks/install-mapnik.sh
+++ tasks/install-mapnik.sh
@@ -6,9 +6,28 @@ set -x
 cd $HOME
 git clone https://github.com/mapnik/mapnik mapnik-3.x
 cd mapnik-3.x
-git checkout v3.0.13
+git checkout v3.0.18
 git submodule update --init
-sudo apt-get install -y python zlib1g-dev clang make pkg-config protobuf-compiler
+sudo apt-get install -y python python-dev curl zlib1g-dev clang make pkg-config protobuf-compiler
+
+# Mason should install these deps in bootstrap.sh:
+#sudo apt-get install -y \
+#    libboost-all-dev \
+#    libicu-dev \
+#    libxml2 libxml2-dev \
+#    libfreetype6 libfreetype6-dev \
+#    libjpeg-dev \
+#    libpng-dev \
+#    libwebp-dev \
+#    libproj-dev \
+#    libtiff-dev \
+#    libcairo2 libcairo2-dev python-cairo python-cairo-dev \
+#    libcairomm-1.0-1v5 libcairomm-1.0-dev \
+#    libgdal1-dev python-gdal \
+#    libsqlite3-dev \
+#    ttf-unifont ttf-dejavu ttf-dejavu-core ttf-dejavu-extra \
+#    git build-essential python-nose
+
 source bootstrap.sh
 ./configure CUSTOM_CXXFLAGS="-D_GLIBCXX_USE_CXX11_ABI=0"
 JOBS=8 make
