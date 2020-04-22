f744377dc32f1b3d99a820c6cb73cf7332ddcdf1
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2020-04-22 16:54:15 +0300

    Add script to launch install manually

---
+++ .circleci/launch-start.sh
@@ -0,0 +1,16 @@
+body='{
+  "parameters": {
+    "manual_start": true
+  }
+}'
+
+echo "Sending body: $body"
+http_code=$(curl -o out -w '%{http_code}' -v -u ${CIRCLECI_TOKEN}: -X POST --header "Content-Type: application/json" -d "$body" https://circleci.com/api/v2/project/github/alvarcarto/alvarcarto-map-server/pipeline)
+
+echo -e "\nResponse:"
+cat out
+
+if [[ $http_code -ne 201 ]]; then
+  echo -e "\n\nCurl response status code was $http_code"
+  exit 1
+fi
