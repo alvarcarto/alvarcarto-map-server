b25a9980403236ef98d616f502c37ce8af011b0b
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2020-04-23 19:46:51 +0300

    Try to fix postgres installation in dedicated server

--- tasks/install-and-configure-postgres.sh
+++ tasks/install-and-configure-postgres.sh
@@ -19,14 +19,16 @@ fi
 sudo apt-get install -y postgresql-10 postgresql-10-postgis-2.4
 
 if [ "$ALVAR_ENV" != "docker" ]; then
-  sudo update-rc.d postgresql enable
+  sudo systemctl enable postgresql.service
+  sudo systemctl stop postgresql.service
+else
+  sudo /etc/init.d/postgresql stop
 fi
 
-# Move data directory to bigger volume
-sudo /etc/init.d/postgresql stop
 echo "Waiting for postgres to stop .. "
 sleep 5
 
+# Move data directory to bigger volume
 # Copy existing postgres data to the new location and create a symlink
 # from the default location to the new one
 sudo cp -r $POSTGRES_DEFAULT_DATA_DIRECTORY $NEW_POSTGRES_DATA_DIRECTORY
@@ -41,7 +43,7 @@ elif [ "$ALVAR_ENV" = "docker" ]; then
     sudo cp confs/postgresql.docker.conf confs/chosen-postgresql.conf
 else
     echo -e "Choosing production postgres config .. "
-    sudo cp confs/postgresql.qa.conf confs/chosen-postgresql.conf
+    sudo cp confs/postgresql.conf confs/chosen-postgresql.conf
 fi
 
 # https://stackoverflow.com/questions/47890233/postgresql-docker-could-not-bind-ipv6-socket-cannot-assign-requested-address
@@ -60,7 +62,12 @@ sudo chown -R postgres:postgres $NEW_POSTGRES_DATA_DIRECTORY
 sudo chmod 700 $NEW_POSTGRES_DATA_DIRECTORY
 sudo chown postgres:postgres /etc/postgresql/10/main/postgresql.conf
 
-sudo /etc/init.d/postgresql start
+if [ "$ALVAR_ENV" != "docker" ]; then
+  sudo systemctl start postgresql.service
+else
+  sudo /etc/init.d/postgresql start
+fi
+
 # In few cases postgres hasn't
 echo "Waiting for postgres to start up .. "
 sleep 5
