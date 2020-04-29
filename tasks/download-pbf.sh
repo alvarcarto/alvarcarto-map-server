5eaa1337c124bb064599498ecae308b8eec515b4
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2020-04-29 16:02:25 +0300

    Switch production PBF_URL to another mirror

--- tasks/download-pbf.sh
+++ tasks/download-pbf.sh
@@ -10,12 +10,19 @@ echo -e "Downloading PBF data ..\n"
 cd $ALVAR_MAP_SERVER_DATA_DIR
 
 if [ "$ALVAR_ENV" = "qa" ] || [ "$ALVAR_ENV" = "docker" ]; then
+    # Example alternatives:
+    # PBF_URL=http://download.geofabrik.de/europe/finland-latest.osm.pbf
+    # PBF_URL=http://download.geofabrik.de/europe-latest.osm.pbf
+    # PBF_URL=http://download.geofabrik.de/europe/spain-latest.osm.pbf
+
     PBF_URL=http://download.geofabrik.de/europe/finland-latest.osm.pbf
 else
-    PBF_URL=http://planet.openstreetmap.org/pbf/planet-latest.osm.pbf
+    # Use another mirror to download planet PBF faster
+    # https://wiki.openstreetmap.org/wiki/Planet.osm#Downloading
+    PBF_URL=https://ftp.fau.de/osm-planet/pbf/planet-latest.osm.pbf
+
+    # Original:
+    #PBF_URL=http://planet.openstreetmap.org/pbf/planet-latest.osm.pbf
 fi
 
-# PBF_URL=http://download.geofabrik.de/europe/finland-latest.osm.pbf
-# PBF_URL=http://download.geofabrik.de/europe-latest.osm.pbf
-# PBF_URL=http://download.geofabrik.de/europe/spain-latest.osm.pbf
 for i in {1..5}; do wget "$PBF_URL" -O import-data.osm.pbf && break || sleep 15; done
