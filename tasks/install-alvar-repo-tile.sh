3b94c7f4c3e4f63f6032305818cd8a2ec4177fa3
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2020-05-05 22:54:19 +0300

    Add crontask to remove zero byte tiles every 8 hours

--- tasks/install-alvar-repo-tile.sh
+++ tasks/install-alvar-repo-tile.sh
@@ -13,3 +13,10 @@ cd alvarcarto-tile-service
 
 nvm use 8.17.0
 npm install
+
+if [ "$ALVAR_ENV" != "docker" ]; then
+  # Sometimes disk cache leaves zero byte tiles on disk
+  echo "Installing cron task to remove zero byte tiles from cache .."
+  # Run every 8th hour, since iterating through whole tiles folder is intensive
+  echo -e "$(crontab -l 2>/dev/null)\n0 */8 * * * /bin/bash -c 'find $ALVAR_MAP_SERVER_DATA_DIR/tiles/ -type f -size 0 -delete'" | crontab -
+fi
