ed596f71df396112d06094bfc3e157d3376e1d51
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2020-05-06 19:02:36 +0300

    Fix png embedded images in render service. Add librsvg logging on by default for render service.

--- tasks/install-alvar-repo-render.sh
+++ tasks/install-alvar-repo-render.sh
@@ -6,6 +6,9 @@ set -x
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
+# This was needed to be able to include png files in SVG
+sudo apt-get install -y librsvg2-2
+
 cd $ALVAR_MAP_SERVER_INSTALL_DIR
 git clone https://alvarcarto-integration:c20a4fe9a8771c17eab5f0470fba434ab2fcf901@github.com/alvarcarto/alvarcarto-render-service.git
 cd alvarcarto-render-service
@@ -21,4 +24,3 @@ npm install
 npm run build-posters
 
 cp -r /usr/share/fonts/truetype/google-fonts/* posters/dist/fonts/
-
